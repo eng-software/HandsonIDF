b55f1abb14430458a7d76f2cf03893ff0c14444c
hcoser <hc.coser@gmail.com>
2024-08-25 18:52:20 -0300

    Alterado exemplo conforme apresentação

--- main/i2c_oled.h
+++ main/i2c_oled.h
@@ -1,16 +1,14 @@
-#ifndef I2C_OLED_H
-#define I2C_OLED_H
-
-#include "driver/i2c_master.h"
+#pragma once
+#ifndef _I2C_OLED_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void i2c_oled_start(i2c_master_bus_handle_t i2c_bus);
+void i2c_oled_init();
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif;
+#endif
