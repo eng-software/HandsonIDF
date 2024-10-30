057064db4a1714622b0b9c7f2a3906b1d9ec0db4
Henrique <hc.coser@gmail.com>
2024-10-29 22:52:12 -0300

    Aplicação separada em tasks

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -11,6 +11,7 @@
 #include "esp_log.h"
 #include "driver/i2c_master.h"
 #include "esp_lvgl_port.h"
+#include "driver/gpio.h"
 #include "lvgl.h"
 #include "i2c_oled.h"
 #include "cbspI2C.h"
@@ -19,22 +20,47 @@
 
 static const char *TAG = "example";
 
-#define I2C_BUS_PORT  0
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+        HARDWARE DEFINITIONS
+*/
+#define I2C_BUS_PORT                  0
 #define EXAMPLE_PIN_NUM_SDA           GPIO_NUM_5
 #define EXAMPLE_PIN_NUM_SCL           GPIO_NUM_4
+#define EXAMPLE_PIN_LED               GPIO_NUM_16               
 
-void example_lvgl_demo_ui();
+/*
+        Components
+*/
 cbspI2C I2CChannel1;
 cBMP280 BMP280;
 cSMP3011 SMP3011;
 
+
+/*
+        TASKS
+*/
+//Prototypes
+void TaskBlink(void *parameter);
+void TaskDisplay(void *parameter);
+void TaskSensors(void *parameter);
+
+//Handlers
+TaskHandle_t taskBlinkHandle   = nullptr;
+TaskHandle_t taskDisplayHandle = nullptr;
+TaskHandle_t taskSensorsHandle = nullptr;
+
+
+
+//Main function
 extern "C"
 void app_main()
 {
+
+    //Setup pin for LED
+    gpio_set_direction(EXAMPLE_PIN_LED, GPIO_MODE_OUTPUT);
+
+    //Setup I2C 0 for display
     ESP_LOGI(TAG, "Initialize I2C bus");
     i2c_master_bus_handle_t i2c_bus = NULL;
     i2c_master_bus_config_t bus_config = {
@@ -52,21 +78,26 @@ void app_main()
     };
     ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus));
 
+    
+    //Setup I2C 1 for sensors
     I2CChannel1.init(I2C_NUM_1, GPIO_NUM_33, GPIO_NUM_32);
     I2CChannel1.openAsMaster(100000);
+
+    //Initialize sensors
     BMP280.init(I2CChannel1);
     SMP3011.init(I2CChannel1);
 
+    //Initialize display
     i2c_oled_start(i2c_bus);
-
-    //ESP_LOGI(TAG, "Display LVGL Scroll Text");
-    // Lock the mutex due to the LVGL APIs are not thread-safe
-    //if (lvgl_port_lock(0)) {
-    //    example_lvgl_demo_ui();
-    //    // Release the mutex
-    //    lvgl_port_unlock();
-    //}
     
+    //Create tasks
+    xTaskCreate( TaskBlink  , "Blink"  , 1024,  nullptr,   tskIDLE_PRIORITY,  &taskBlinkHandle   );  
+    xTaskCreate( TaskSensors, "Sensors", 2048,  nullptr,   tskIDLE_PRIORITY,  &taskSensorsHandle );    
+    xTaskCreate( TaskDisplay, "Display", 4096,  nullptr,   tskIDLE_PRIORITY,  &taskDisplayHandle );
+}
+
+void TaskDisplay(void *parameter)
+{
 
     lvgl_port_lock(0);
     lv_obj_t *scr = lv_disp_get_scr_act(nullptr);
@@ -102,9 +133,7 @@ void app_main()
     lvgl_port_unlock();
 
     while(1)
-    {
-        BMP280.poll();
-        SMP3011.poll();
+    {      
 
         lvgl_port_lock(0);
         lv_label_set_text_fmt(labelPressure, "%6.0fPa", SMP3011.getPressure());
@@ -113,8 +142,28 @@ void app_main()
         lv_label_set_text_fmt(labelTemperatureAtm, "%6.2fC", BMP280.getTemperature());
         lvgl_port_unlock();
 
-        vTaskDelay(20/portTICK_PERIOD_MS);
-    }
-
-    
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }    
 }
+
+void TaskBlink(void *parameter)
+{
+    while(1)
+    {
+        gpio_set_level(EXAMPLE_PIN_LED, 1); 
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        gpio_set_level(EXAMPLE_PIN_LED, 0); 
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+}
+
+void TaskSensors(void *parameter)
+{
+    while(1)
+    {
+        BMP280.poll();
+        SMP3011.poll();
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
+}
+
